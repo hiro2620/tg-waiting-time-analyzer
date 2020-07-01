73955d781141f95b53868b353ba126c714818ef7
hiro2620 <ihiro2620@gmail.com>
2020-07-01 16:50:45 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+class Train:
+
+	def __init__(self, arrival_time, , people_cnt, standard_deviation=1):
+
+		self.station = station
+		self.people_cnt = people_cnt
+		self.standard_deviation = standard_deviation
+
+
+	def 
+
+
+class PeopleFlow:
+	pass
+
+
+class Thermography:
+	pass
+
+
+class WatingLine:
+	pass
